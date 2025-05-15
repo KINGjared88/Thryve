125ca7d212f77395f349cf106ace604589c6c7bd
KINGjared88 <jared@urban48.com>
2025-05-14 18:37:08 -0700

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: '2rem' }}>
+      <h1>Thryve Chatbot App</h1>
+      <p>This is the homepage. The chatbot widget will appear on embedded websites.</p>
+    </div>
+  );
+}
