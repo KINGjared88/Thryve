f60cdeca533ebc04af852cd3b6b9f19c036c7213
KINGjared88 <jared@urban48.com>
2025-05-14 18:47:37 -0700

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Thryve Chatbot App</h1>
-      <p>This is the homepage. The chatbot widget will appear on embedded websites.</p>
-    </div>
-  );
-}
